1b8f51e0a2f85d3072e0fc543f1b9e41325ae0f7
Maxim Monin <maximmonin@gmail.com>
2020-10-30 21:44:09 +0000

    added stress tests

---
+++ test/zeebe_stress.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker pull williamyeh/wrk
+
+myip=$(hostname -I | cut -d' ' -f1)
+# run 100 connection within 10 threads during 10 seconds (spam start process)
+docker run --rm -v `pwd`:/data williamyeh/wrk -s post.lua -t10 -c100 -d10s http://$myip:2700/zeebe/process/payment-retrieval/start

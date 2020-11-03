8c2520181b1decf998ee3a3e2d5b7b3a339a7eb3
Maxim Monin <maximmonin@gmail.com>
2020-11-03 16:07:36 +0000

    final

--- test/camunda_stress.sh
+++ test/camunda_stress.sh
@@ -3,5 +3,5 @@
 docker pull williamyeh/wrk
 
 myip=$(hostname -I | cut -d' ' -f1)
-# run 100 connection within 10 threads during 10 seconds (spam start process)
+# run 100 connections within 10 threads during 10 seconds (spam start process)
 docker run --rm -v `pwd`:/data williamyeh/wrk -s post.lua -t10 -c100 -d10s http://$myip:2700/camunda/process/payment-retrieval/start
